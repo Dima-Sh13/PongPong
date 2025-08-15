3e947583d013c94c980a047bedbb3c452eb1c00c
Dima Shalupnya <dshalupnya@gmail.com>
2025-08-15 16:50:00 +0200

    Añado otro diseño de campo para seleccionar

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+from PongPingApp.utils import *
+
 def rebote(posicionX_raquetaI, poscionX_raquetaD,posicionY_raquetaI, posicionY_raquetaD,posicion_balon,vx_balon):
     dir_balon=posicion_balon[0]+vx_balon
     valores_rebote_raqI=[]
@@ -20,7 +22,18 @@ def rebote(posicionX_raquetaI, poscionX_raquetaD,posicionY_raquetaI, posicionY_r
     if posicion_balon[0] ==poscionX_raquetaD and posicion_balon[1]>= posicionY_raquetaD[0] and posicion_balon[1] <= posicionY_raquetaD[1]:
         vx_balon*= -1
     
-
+def draw_field_tennis(mainScreen):
+        mainScreen.fill((245, 73, 39))       
+        pg.draw.line(mainScreen,color_blanco,(0,55),(screenX,55),7)        
+        pg.draw.line(mainScreen,color_blanco,(3,0),(3,screenY),7)
+        pg.draw.line(mainScreen,color_blanco,(3,screenY-55),(screenX,screenY-55),7)
+        pg.draw.line(mainScreen,color_blanco,(screenX-4,0),(screenX-4,screenY),7)
+        pg.draw.line(mainScreen,color_blanco,(0,3),(screenX,3),7)
+        pg.draw.line(mainScreen,color_blanco,(0,screenY-3),(screenX,screenY-3),7)
+        pg.draw.line(mainScreen,color_blanco,(screenX//4-50,55),(screenX//4-50,screenY-55),7)
+        pg.draw.line(mainScreen,color_blanco,(screenX//4*3+50,55),(screenX//4*3+50,screenY-55),7)
+        pg.draw.line(mainScreen,color_blanco,(screenX//4*2,0),(screenX//4*2,screenY),7)
+        pg.draw.line(mainScreen,color_blanco,(screenX//4-50,screenY//2),(screenX//4*3+50,screenY//2),7)
 
 
    
